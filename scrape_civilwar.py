50b1d9a080eaa2c6c2f4709de9a79532c5d52dec
cbarne02 <Christopher.Barnett@tufts.edu>
2018-10-23 18:05:02 -0400

    add scraper for civil war photos text add result file

--- scrape_civilwar.py
+++ scrape_civilwar.py
@@ -11,44 +11,19 @@ from os.path import basename
 
 class CivilWarScraper:
     def __init__(self):
-        self.contents = {}
+        self.pages = {}
         self.target_dir = './data/texts/civil_war_photos'
         self.html_file = './data/texts/civil_war_photos/43922-h.htm'
 
-    def read_contents(self):
-        self.contents = {}
-        with open(self.html_file, 'r') as f:
-            text = f.read()
-            x = Soup(text, 'html.parser')
-            a = x.findAll('hr')
-            volumes = ['1818v1', '1818v2', '1818v3']
-            index = 0
-            for i in a:
-                if 'frankenstein' in i.get('href'):
-                    link = i.get('href')
-                    volume = self.parse_vol(link)
-                    abs_url = urllib.parse.urljoin(self.contents_url, link)
-                    if volume in volumes:
-                        chapter = {
-                            'link': abs_url,
-                            'label': i.get_text(),
-                            'chapter_index': index,
-                            'volume': volume
-                        }
-                        self.contents[index] = chapter
-                        index += 1
+    def read_pages(self):
+        self.pages = {}
+        soup = self.load_html(self.html_file)
+        self.pages = self.get_pages(soup)
+        self.persist_pages()
 
-    def read_stored_contents(self):
-        path = os.path.join(self.target_dir, 'contents.json')
-        with open(path, 'r') as f:
-            self.contents = json.loads(f.read())
-
-    def parse_vol(self, link):
-        return link.split('/')[-2]
-
-    def persist_contents(self):
-        path = os.path.join(self.target_dir, 'contents.json')
-        self.write_json(self.contents, path)
+    def persist_pages(self):
+        path = os.path.join(self.target_dir, 'pages.json')
+        self.write_json(self.pages, path)
 
     def write_json(self, map, path):
         with open(path, 'w') as f:
@@ -56,44 +31,82 @@ class CivilWarScraper:
 
     def load_html(self, path):
         html = ''
-        with open(path, 'r') as f:
+        with open(path, 'rb') as f:
             html = f.read()
         return Soup(html, 'html.parser')
 
-    def get_text_map(self, soup):
-        text_map = {}
-        chapter = soup.find('div', 'field-type-text-with-summary')
-        if chapter is None:
-            return text_map
-        chapter = chapter.find('ol')
-        if chapter is None:
-            print('no ol found')
-            return text_map
-        paragraphs = chapter.find_all('li', recursive=False)
+    def get_page_id(self, span):
+        return span.find('a').get('id')
 
-        for pindex, p in enumerate(paragraphs):
-            text_map[pindex] = {'text': p.get_text().strip()}
-            anchors = p.find_all('a')
-            for a in anchors:
-                pid = a.get('id')
-                if pid is not None and pid.startswith('p'):
-                    text_map[pindex]['pid'] = pid
-        return text_map
+    def parse_text(self, soup):
+        if soup.name in ['hr']:
+            return {}
+        if soup.name == 'p':
+            if 'indent' in soup.get('class'):
+                text = soup.get_text()
+                text = text.replace('\n', ' ').replace('\r', ' ').replace('  ', ' ')
+                # print(text)
+                return {'text': text}
+        elif soup.name == 'div':
+            caption = soup.find('div', 'caption')
+            if caption is not None:
+                text = caption.get_text()
+                text = text.replace('\r\n', ' ').replace('\n\n', '\n')
+                text = text.lstrip('\n')
+                text = text.replace('\n', '. ').replace('  ', ' ')
+                return {'caption': text}
+        return {}
 
-    def get_metadata(self, soup):
-        metadata = {}
-        meta = soup.find_all('meta')
-        for m in meta:
-            if m.has_attr('name') and m.get('name').startswith('dcterms'):
-                metadata[m.get('name')] = m.get('content')
-        return metadata
+    def get_pages(self, soup):
+        page_idx = 0
+        firstpage = soup.find('span', 'pagenum')
+        m = firstpage.parent
+        pages = {
+            page_idx: {
+                'html': str(m),
+                'text': '',
+                'caption': '',
+                'page_id': self.get_page_id(firstpage)
+            }
+        }
+        while True:
+            try:
+                m = m.find_next_sibling()
+                x = m.find('span', 'pagenum')
+                if x is not None and x != -1:
+                    page_id = self.get_page_id(x)
+                    if int(page_id[4:]) > 114:
+                        break
+                    page_idx += 1
+                    pages[page_idx] = {'html': '', 'text': '', 'caption': '', 'page_id': page_id}
+                else:
+                    # skip parsing text if pagenum node
+                    parsed = self.parse_text(m)
+                    if 'text' in parsed:
+                        pages[page_idx]['text'] += parsed['text']
+                    elif 'caption' in parsed:
+                        pages[page_idx]['caption'] += parsed['caption']
 
-    def write_chapter_json(self):
+                pages[page_idx]['html'] += str(m)
+
+            except AttributeError as e:
+                break
+        return pages
+
+    def get_paragraphs(self):
+        for k, v in self.pages.items():
+            page = Soup(v['html'], 'html.parser')
+            text = page.get_text()
+            text = text.replace('\n', ' ').replace('\r', ' ').replace('  ', ' ')
+
+            v['text'] = text
+        self.persist_pages()
+
+    def write_page_json(self):
         for k, v in self.contents.items():
             chapter = copy.deepcopy(v)
             soup = self.load_html(v.get('file_path'))
             chapter['textmap'] = self.get_text_map(soup)
-            chapter['metadata'] = self.get_metadata(soup)
             path = os.path.join(self.target_dir, basename(v.get('file_path')).replace('.html', '.json'))
             self.write_json(chapter, path)
 
